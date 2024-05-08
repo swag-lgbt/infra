c67be28e2143beb9676d6d1ed546f5471015b33c
Cass Fridkin <cass@swag.lgbt>
2024-05-08 10:33:42 -0400

    Do a billion things

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,25 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import { config, default as tseslint } from 'typescript-eslint';
-import eslint from '@eslint/js';
 
-import path from 'node:path';
+import { config, default as tseslint } from "typescript-eslint";
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import packageJson from "eslint-plugin-package-json/configs/recommended";
+
+import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname
+	baseDirectory: __dirname,
 });
 
 export default config(
-  eslint.configs.all,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  ...compat.extends("plugin:qwik/recommended")
-)
+	eslint.configs.all,
+	...tseslint.configs.strict,
+	...tseslint.configs.stylistic,
+	...compat.extends("plugin:qwik/recommended"),
+	packageJson,
+	eslintConfigPrettier,
+);
