18e33dcb04aae068b4f49007d7782aabd0cb2302
Cass Fridkin <cass@swag.lgbt>
2024-05-18 15:54:12 -0400

    Remove digitalocean resources

--- main.tf
+++ main.tf
@@ -36,28 +36,24 @@ terraform {
 # 
 # 1password is configured via the OP_SERVICE_ACCOUNT_TOKEN environment variable
 
-provider "digitalocean" {
-  token = module.data.onepassword.credentials.digitalocean_access_token
-}
-
 provider "cloudflare" {
   api_token = module.data.onepassword.credentials.cloudflare_api_token
 }
 
 
-provider "kubernetes" {
-  host                   = module.infra.kubernetes.host
-  token                  = module.infra.kubernetes.token
-  cluster_ca_certificate = module.infra.kubernetes.cluster_ca_certificate
-}
+# provider "kubernetes" {
+#   host                   = module.infra.kubernetes.host
+#   token                  = module.infra.kubernetes.token
+#   cluster_ca_certificate = module.infra.kubernetes.cluster_ca_certificate
+# }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.infra.kubernetes.host
-    token                  = module.infra.kubernetes.token
-    cluster_ca_certificate = module.infra.kubernetes.cluster_ca_certificate
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.infra.kubernetes.host
+#     token                  = module.infra.kubernetes.token
+#     cluster_ca_certificate = module.infra.kubernetes.cluster_ca_certificate
+#   }
+# }
 
 # MODULES
 #
@@ -72,49 +68,49 @@ module "data" {
 
 # The "infra" module owns infrastructure that isn't tied to a particular project,
 # for example a kubernetes cluster
-module "infra" {
-  source = "./infra"
+# module "infra" {
+#   source = "./infra"
 
-  region                 = "nyc3"
-  onepassword_vault_uuid = module.data.onepassword.vault_uuid
+#   region                 = "nyc3"
+#   onepassword_vault_uuid = module.data.onepassword.vault_uuid
 
-  kubernetes = {
-    ha             = false
-    version_prefix = "1.29"
+#   kubernetes = {
+#     ha             = false
+#     version_prefix = "1.29"
 
-    node = {
-      size = "c-2"
+#     node = {
+#       size = "c-2"
 
-      pool = {
-        auto_scale = {
-          from = 1
-          to   = 3
-        }
-      }
-    }
+#       pool = {
+#         auto_scale = {
+#           from = 1
+#           to   = 3
+#         }
+#       }
+#     }
 
-    maintenance_policy = {
-      day        = "saturday"
-      start_time = "08:00"
-    }
-  }
+#     maintenance_policy = {
+#       day        = "saturday"
+#       start_time = "08:00"
+#     }
+#   }
 
-  postgres = {
-    version          = 16
-    storage_size_gib = 30
+#   postgres = {
+#     version          = 16
+#     storage_size_gib = 30
 
-    nodes = {
-      primary = {
-        size = "db-s-1vcpu-2gb"
-      }
-    }
+#     nodes = {
+#       primary = {
+#         size = "db-s-1vcpu-2gb"
+#       }
+#     }
 
-    maintenance_policy = {
-      day        = "sunday"
-      start_time = "08:00"
-    }
-  }
-}
+#     maintenance_policy = {
+#       day        = "sunday"
+#       start_time = "08:00"
+#     }
+#   }
+# }
 
 # The "apps" module contains all swagLGBT related applications, which all own their own tofu.
 module "apps" {
