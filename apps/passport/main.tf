b3d48d876b64965de7821f8b5d4ead994a1c0e51
Cass Fridkin <cass@swag.lgbt>
2024-05-27 20:25:09 -0400

    Use the new 1password provider + use preview bindings

--- apps/passport/main.tf
+++ apps/passport/main.tf
@@ -6,39 +6,55 @@ terraform {
     }
     onepassword = {
       source  = "1Password/onepassword"
-      version = "~> 1.4"
+      version = "~> 2.0"
     }
   }
 }
 
-locals {
-  // use a consistent node version between local development and cloudflare pages
-  root_package_json = jsondecode(file("${path.root}/package.json"))
-  node_version      = local.root_package_json.volta.node
-  pnpm_version      = local.root_package_json.volta.pnpm
+#
+# This project deals with Single-Sign on, and our SSO provider is Stytch.
+# So we'll want to get our credentials from them to use in other config stuff here.
+#
+
+data "onepassword_item" "stytch" {
+  vault = var.onepassword.vault_uuid
+  title = "Stytch Credentials"
 }
 
-# For production OAuth, we can use the swagLGBT domain instead of a stytch.com one
-data "onepassword_item" "stytch_api_subdomain" {
-  vault = var.onepassword.vault_uuid
-  title = "Stytch API Subdomain"
+#
+# This section deals with setting our custom domain for OAuth in prod.
+# There's no dedicated docs about it, but they have a feature where you can
+# create a CNAME record from `api.stytch.<YOUR_DOMAIN>` to `<RANDOM_WORDS>.customers.stytch.com`
+# and it'll change the "stytch.com" shown in OAuth logins to "swag.lgbt"
+#
+
+locals {
+  oauth_custom_domain_section_index = index(data.onepassword_item.stytch.section.*.label, "OAuth Custom Domain")
+  oauth_custom_domain_section       = data.onepassword_item.stytch.section[local.oauth_custom_domain_section_index].field
+  oauth_custom_domain_name_index    = index(local.oauth_custom_domain_section.*.label, "name")
+  oauth_custom_domain_value_index   = index(local.oauth_custom_domain_section.*.label, "value")
+
+  # These aren't actually sensitive, since anyone can see them by just getting DNS records for swag.lgbt
+  oauth_custom_domain = {
+    name  = nonsensitive(local.oauth_custom_domain_section[local.oauth_custom_domain_name_index].value),
+    value = nonsensitive(local.oauth_custom_domain_section[local.oauth_custom_domain_value_index].value)
+  }
 }
 
 resource "cloudflare_record" "stytch" {
   zone_id = var.cloudflare.zone_id
-  name    = "api.stytch"
-  value   = data.onepassword_item.stytch_api_subdomain.username
+  name    = local.oauth_custom_domain.name
+  value   = local.oauth_custom_domain.value
   type    = "CNAME"
   proxied = false
   ttl     = 3600
 }
 
-
-resource "cloudflare_pages_domain" "passport" {
-  account_id   = var.cloudflare.account_id
-  domain       = "${var.passport.subdomain}.swag.lgbt"
-  project_name = cloudflare_pages_project.passport.name
-}
+#
+# Now we're into setting up domain names for our Cloudflare Pages project.
+# We need two resources, one to create a subdomain, and one to assign
+# that subdomain to our project.
+#
 
 resource "cloudflare_record" "passport" {
   zone_id = var.cloudflare.zone_id
@@ -47,6 +63,46 @@ resource "cloudflare_record" "passport" {
   type    = "CNAME"
   proxied = true
   ttl     = 1
+}
+
+resource "cloudflare_pages_domain" "passport" {
+  account_id   = var.cloudflare.account_id
+  domain       = "${var.passport.subdomain}.swag.lgbt"
+  project_name = cloudflare_pages_project.passport.name
+}
+
+#
+# Now we're into configuring the cloudflare pages project proper
+#
+
+# Use consistent versions for Node and PNPM between local development (volta)
+# and production (cloudflare pages) by setting environment variables
+# that read off of what we've got in prod
+locals {
+  root_package_json = jsondecode(file("${path.root}/package.json"))
+  node_version      = local.root_package_json.volta.node
+  pnpm_version      = local.root_package_json.volta.pnpm
+}
+
+# Pull our stytch credentials from 1password and make them available to our pages project
+locals {
+  prod_env_section_index = index(data.onepassword_item.stytch.section.*.label, "Live Environment")
+  prod_env_section       = data.onepassword_item.stytch.section[local.prod_env_section_index].field
+
+  preview_env_section_index = index(data.onepassword_item.stytch.section.*.label, "Test Environment")
+  preview_env_section       = data.onepassword_item.stytch.section[local.prod_env_section_index].field
+
+  stytch_credentials = {
+    prod = {
+      public_token       = local.prod_env_section[index(local.prod_env_section.*.label, "Public token")].value
+      oauth_redirect_uri = local.prod_env_section[index(local.prod_env_section.*.label, "OAuth redirect URI")].value
+    }
+
+    preview = {
+      public_token       = local.preview_env_section[index(local.prod_env_section.*.label, "Public token")].value
+      oauth_redirect_uri = local.preview_env_section[index(local.prod_env_section.*.label, "OAuth redirect URI")].value
+    }
+  }
 }
 
 resource "cloudflare_pages_project" "passport" {
@@ -76,26 +132,28 @@ resource "cloudflare_pages_project" "passport" {
   deployment_configs {
     production {
       environment_variables = {
-        # Runtime vars
-        OAUTH_REDIRECT_API = "https://api.stytch.swag.lgbt/v1/oauth/callback/oauth-callback-live-c24db04e-7018-4646-b66e-9bdce7194b32"
-
         # Build vars
         NODE_VERSION = local.node_version
         PNPM_VERSION = local.pnpm_version
+      }
+
+      secrets = {
+        OAUTH_REDIRECT_URI  = local.stytch_credentials.prod.oauth_redirect_uri
+        STYTCH_PUBLIC_TOKEN = local.stytch_credentials.prod.public_token
       }
     }
 
     preview {
       environment_variables = {
-        # Runtime vars
-        OAUTH_REDIRECT_API = "https://test.stytch.com/v1/oauth/callback/oauth-callback-test-2b5d454f-e05d-498d-9d10-4abeb5a50591"
-
         # Build vars
         NODE_VERSION = local.node_version
         PNPM_VERSION = local.pnpm_version
       }
+
+      secrets = {
+        OAUTH_REDIRECT_URI  = local.stytch_credentials.preview.oauth_redirect_uri
+        STYTCH_PUBLIC_TOKEN = local.stytch_credentials.preview.public_token
+      }
     }
   }
-
-
 }
--- apps/passport/vite.config.ts
+++ apps/passport/vite.config.ts
@@ -1,9 +1,9 @@
-/**
- * This is the base config for vite.
- * When building, the adapter config is used which loads this file and extends it.
- */
+import {
+	type QwikCityVitePluginOptions,
+	qwikCity,
+} from "@builder.io/qwik-city/vite";
 import { type UserConfig, defineConfig } from "vite";
-import { qwikCity } from "@builder.io/qwik-city/vite";
+import { execa } from "execa";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -15,18 +15,116 @@ if ("dependencies" in pkg) {
 	);
 }
 
+const getNestedChildProperty = (
+	parent: unknown,
+	...keys: (string | number | symbol)[]
+): unknown => {
+	if (keys.length === 0) {
+		return parent;
+	}
+
+	const [firstKey, ...restKeys] = keys;
+
+	if (Array.isArray(parent) && typeof firstKey === "number") {
+		const nextChild = parent[firstKey] as unknown;
+		return getNestedChildProperty(nextChild, ...restKeys);
+	} else if (
+		typeof parent === "object" &&
+		parent !== null &&
+		firstKey in parent
+	) {
+		const nextChild = (parent as Record<typeof firstKey, unknown>)[firstKey];
+		return getNestedChildProperty(nextChild, ...restKeys);
+	}
+
+	throw new Error(
+		`Cannot index into ${JSON.stringify(parent)} with key ${firstKey.toString()}`,
+	);
+};
+
+interface CloudflarePagesDeploymentConfig {
+	environment_variables: Record<string, string>;
+	secrets: Record<string, string>;
+}
+
+const isCloudflarePagesDeploymentConfig = (
+	deploymentConfig: unknown,
+): deploymentConfig is CloudflarePagesDeploymentConfig =>
+	typeof deploymentConfig === "object" &&
+	deploymentConfig !== null &&
+	"environment_variables" in deploymentConfig &&
+	typeof deploymentConfig.environment_variables === "object" &&
+	deploymentConfig.environment_variables !== null &&
+	"secrets" in deploymentConfig &&
+	typeof deploymentConfig.secrets === "object" &&
+	deploymentConfig.secrets !== null;
+
+const parseTofuOutputsIntoCloudflareBindings = (outputs: unknown): Env => {
+	if (typeof outputs !== "object") {
+		throw new Error(`Expected an object, received ${typeof outputs} instead.`);
+	}
+
+	if (outputs === null) {
+		throw new Error(`Unexpected null attempting to parse Tofu outputs`);
+	}
+
+	const nestedKeys = [
+		"apps",
+		"value",
+		"passport",
+		"preview_deployment_config",
+		0,
+	];
+	const previewDeploymentConfig = getNestedChildProperty(
+		outputs,
+		...nestedKeys,
+	);
+	if (!isCloudflarePagesDeploymentConfig(previewDeploymentConfig)) {
+		throw new Error(
+			`Expected output ${nestedKeys.join(".")} to be a Cloudflare Pages deployment config, but it wasn't.`,
+		);
+	}
+
+	return {
+		NODE_VERSION: previewDeploymentConfig.environment_variables.NODE_VERSION,
+		OAUTH_REDIRECT_API: previewDeploymentConfig.secrets.OAUTH_REDIRECT_API,
+		PNPM_VERSION: previewDeploymentConfig.environment_variables.PNPM_VERSION,
+		STYTCH_PUBLIC_TOKEN: previewDeploymentConfig.secrets.STYTCH_PUBLIC_TOKEN,
+	};
+};
+
+const getPreviewBindings = async (): Promise<
+	QwikCityVitePluginOptions["platform"]
+> => {
+	// In dev mode, we need to inject variables from terraform into our app.
+	// In production, these are set through terraform's configuration of cloudflare pages.
+	const { stdout, exitCode, stderr } = await execa`just tofu output -json`;
+	if (exitCode && exitCode !== 0) {
+		throw new Error(
+			`Error attempting to get tofu output to set dev vars:\n\n${stderr}`,
+		);
+	}
+
+	const tofuOutputs = JSON.parse(stdout) as unknown;
+	const env = parseTofuOutputsIntoCloudflareBindings(tofuOutputs);
+	return { env };
+};
+
 /**
  * Note that Vite normally starts from `index.html` but the qwikCity plugin makes start at `src/entry.ssr.tsx` instead.
  */
-export default defineConfig(
-	(): UserConfig => ({
+export default defineConfig(async ({ command }): Promise<UserConfig> => {
+	// Set up mock cloudflare bindings in dev mode, leave it be in prod
+	const platform = command === "serve" ? await getPreviewBindings() : undefined;
+
+	return {
 		// This tells Vite which dependencies to pre-build in dev mode.
 		optimizeDeps: {
 			// Put problematic deps that break bundling here, mostly those with binaries.
 			// For example ['better-sqlite3'] if you use that in server functions.
 			exclude: [],
 		},
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		plugins: [qwikCity({ platform }), qwikVite(), tsconfigPaths()],
 		preview: {
 			headers: {
 				// Do cache the server response in preview (non-adapter production build)
@@ -39,5 +137,5 @@ export default defineConfig(
 				"Cache-Control": "public, max-age=0",
 			},
 		},
-	}),
-);
+	};
+});
